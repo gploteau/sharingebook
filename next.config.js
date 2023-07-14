d11d39db8fc0093f9a2fc771ff7b07254c4483b0
Gilles <gilles@kadolis.com>
2023-07-14 22:14:48 +0200

    remove distDir

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
     // Optional: Add a trailing slash to all paths `/about` -> `/about/`
     // trailingSlash: true,
     // Optional: Change the output directory `out` -> `dist`
-    distDir: 'build',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
