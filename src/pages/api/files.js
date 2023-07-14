0d6af4dd1adbd9a8bb8afef77b80ead54542f84f
Gilles <gilles@kadolis.com>
2023-07-14 22:37:45 +0200

    use files server distant

--- src/pages/api/files.js
+++ src/pages/api/files.js
@@ -1,12 +1,7 @@
 import fs from 'fs'
 import path from 'path'
-import * as mm from 'music-metadata';
-const { getAudioDurationInSeconds } = require('get-audio-duration')
-const md5 = require('md5');
 
 const getTracks = async () => {
-
-    const dirRelativeToPublicFolder = 'sound';
 
     const db_file = path.resolve('./public', 'db.json');
 
@@ -16,40 +11,7 @@ const getTracks = async () => {
         db = JSON.parse(fs.readFileSync(db_file));
     }
 
-    const dir = path.resolve('./public', dirRelativeToPublicFolder);
-
-    const filenames = fs.readdirSync(dir);
-
-    if(filenames.length === db.length) {
-        return db;
-    }
-
-    const tracks = await Promise.all(filenames.map(name => {
-        path.join('/', dirRelativeToPublicFolder, name)
-        const metaData = mm.parseFile(path.join(dir, name));
-        const duration = getAudioDurationInSeconds(path.join(dir, name));
-
-        return duration.then((duration) => {
-            return metaData.then((metadata) => {
-
-                let cover = metadata.common.picture ?
-                    'data:' + metadata.common.picture[0].format + ';base64,' + Buffer.from(metadata.common.picture[0].data).toString('base64') : ''
-
-                return {
-                    id: md5(name),
-                    duration: duration,
-                    file: path.join('/', dirRelativeToPublicFolder, name),
-                    title: metadata.common.title || name,
-                    author: metadata.common.artist,
-                    cover : cover
-                };
-            });
-        })
-    }));
-
-    fs.writeFileSync(db_file, JSON.stringify(tracks));
-
-    return tracks;
+    return db;
 }
 
 export default async (req, res) => {
