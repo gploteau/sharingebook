b8c2cf610af5724893b0fa31ffe4c682a2b8d647
Gilles <gilles.ploteau@gmail.com>
2023-12-10 15:31:12 +0100

    Autocommit-20231210-1531

--- deploy.cmd
+++ deploy.cmd
@@ -17,14 +17,10 @@ popd
 echo Server Sync
 git add --all
 git add build -f
-set CUR_YYYY=%date:~10,4%
-set CUR_MM=%date:~4,2%
-set CUR_DD=%date:~7,2%
 set CUR_HH=%time:~0,2%
 if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
 set CUR_NN=%time:~3,2%
-set CUR_SS=%time:~6,2%
-set comment=Autocommit-%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
+set comment=Autocommit-%DATE:~-4%%DATE:~-7,2%%DATE:~-10,2%-%CUR_HH%%CUR_NN%
 git commit -m "%comment%"
 git push github main --force-with-lease
 
