037f53a9eab1c9b92aff51c0d4f865f1776fe05e
Gilles <gilles.ploteau@gmail.com>
2023-12-10 15:23:59 +0100

    Autocommit-2/02-152359

--- deploy.cmd
+++ deploy.cmd
@@ -6,7 +6,6 @@ echo Deploy Started
 pushd %DEPLOYMENT_SOURCE%
 
 echo Build React App
-export CI=true
 call npm run build
 
 IF %ERRORLEVEL% NEQ 0 (
@@ -16,10 +15,18 @@ goto error
 popd
 
 echo Server Sync
+git add --all
 git add build -f
-SET comment=Autocommit-%date%-%time%
+set CUR_YYYY=%date:~10,4%
+set CUR_MM=%date:~4,2%
+set CUR_DD=%date:~7,2%
+set CUR_HH=%time:~0,2%
+if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
+set CUR_NN=%time:~3,2%
+set CUR_SS=%time:~6,2%
+set comment=Autocommit-%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
 git commit -m "%comment%"
-git push origin prod --force-with-lease
+git push github main --force-with-lease
 
 goto end
 
